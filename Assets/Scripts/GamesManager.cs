72cf7c1aca092f12d072853631dc69efabc6c87f
Olega <oleh.valdemar@gmail.com>
2023-03-14 19:14:35 +0200

    upload project

---
+++ Assets/Scripts/GamesManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GamesManager : MonoBehaviour
+{
+    [SerializeField] private Button tapToStart_btn;
+    [SerializeField] private Text money_txt;
+    [SerializeField] private GameObject menuUI;
+    [SerializeField] private PlayerManager playerManager;
+    [SerializeField] private GameObject loseGameUI;
+    [SerializeField] private GameObject winGameUI;
+    [SerializeField] private Button restartGame_btn;
+
+    private void Awake()
+    {
+        money_txt.text = PlayerPrefs.GetInt("money", 0).ToString();
+    }
+    public void PressStart()
+    {
+        menuUI.SetActive(false);
+        playerManager.onGameState();
+    }
+    public void LoseGame()
+    {
+        loseGameUI.SetActive(true);
+    }
+    public void ClickRestart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
